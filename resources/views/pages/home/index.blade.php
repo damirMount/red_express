d23dd47bc4a3053c55fd0a339325b98b58c6229f
Frogy <nurs.axe@gmail.com>
2022-06-02 12:30:46 +0600

    fix and add header

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+
+@section('content')
+    <section>
+        <div class="container-fluid first-block">
+
+            <div class="container">
+
+            </div>
+        </div>
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home.index');
 });
 Route::name('admin.')
     ->middleware('admin')
