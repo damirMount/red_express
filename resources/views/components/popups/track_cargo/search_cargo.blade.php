269484fd66ab73c480a9680ba7605d6580504fbf
Frogy <nurs.axe@gmail.com>
2022-06-13 17:41:25 +0600

    create popups and add fonts

---
+++ resources/views/components/popups/track_cargo/search_cargo.blade.php
@@ -0,0 +1,52 @@
+  <!-- Modal -->
+    <div
+        class="modal fade modal-search"
+        id="modalSearch"
+        tabindex="2"
+        aria-labelledby="modalSearch"
+        aria-hidden="true"
+    >
+    <div class="modal-dialog modal-sm modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header border-0">
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="first-step">
+                <p class="modal-text text-center">
+                    Номер накладной
+                </p>
+                <div class="text-center">
+
+                <input
+                    type="text"
+                    class="search-input"
+                >
+                <button class="search-cargo" id="Search">
+                    Найти
+                </button>
+                </div>
+            </div>
+            <div class="second-steps d-none">
+                @include('.components.popups.track_cargo.result')
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+@section('scripts')
+  <script>
+        const searchBtn = document.querySelector('#Search');
+        const firstStep = document.querySelector('.modal-search .first-step');
+        const secondStep = document.querySelector('.modal-search .second-step');
+
+        searchBtn.addEventListener('click', () => {
+            console.log('work');
+            firstStep.classList.toggle('d-none');
+            secondStep.classList.toggle('d-none');
+        })
+
+  </script>
+@endsection
+<!-- Including in app.blade.php -->
--- resources/views/pages/news/view_news.blade.php
+++ resources/views/pages/news/view_news.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.app')
+@section('styles')
+    <link rel="stylesheet" href={{ asset('css/view_news.css')}}>
+@endsection
 
 @section('content')
-    <div class="container">
+    <div class="container red-express__news">
         <div class="row justify-content-between">
+            <h1>
+                Заголовок новости
+            </h1>
             <div class="col-8">
                 @include('pages.news.components.blog')
             </div>
