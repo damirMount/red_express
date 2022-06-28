431e4dfeb28b5531bc0c1e62cd70a47858358c8f
Frogy <nurs.axe@gmail.com>
2022-06-28 19:16:00 +0600

    create new searching cargo modal in search_cargo.blade.php

--- resources/views/components/popups/track_cargo/search_cargo.blade.php
+++ resources/views/components/popups/track_cargo/search_cargo.blade.php
@@ -7,29 +7,83 @@
     aria-hidden="true"
 >
     <div class="modal-dialog modal-sm modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header border-0">
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <div class="first-step">
-                <p class="modal-text text-center">
-                    Номер накладной
-                </p>
-                <div class="text-center">
-
-                <input
-                    type="text"
-                    class="search-input"
-                >
-                <button class="search-cargo" data-bs-target="#modalResult" data-bs-toggle="modal" id="Search">
-                    Найти
-                </button>
+        <div class="modal-content border-0 shadow-sm">
+            <div class="modal-header px-3 border-bottom-0">
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-        </div>
-      </div>
+            <div class="modal-body px-3">
+                 <div class="row mx-0">
+                   <h2 class="red-modal-title">
+                     Онлайн-отслеживание груза
+                   </h2>
+                   <div class="col-6 mx-auto red-modal-line"></div>
+                   <div class="col-12 mt-4 px-0">
+                     <p class="red-modal-text">
+                       Номер накладной
+                     </p>
+                     <form class="red-modal-form d-flex justify-content-between">
+                       <input type="text" class="search-input">
+                       <button class="search-btn btn">
+                         Найти
+                       </button>
+                     </form>
+                   </div>
+                   <div class="col-12 px-0 my-4 red-modal-results">
+                     <p class="head">
+                       Результат:
+                     </p>
+                     <div class="d-flex result justify-content-between">
+                       <div class="result-name">Груз:</div>
+                       <div class="result-text">486060</div>
+                     </div>
+                     <div class="d-flex result justify-content-between">
+                       <div class="result-name">Статус:</div>
+                       <div class="result-text status">Ожидается</div>
+                     </div>
+                     <div class="d-flex result justify-content-between">
+                       <div class="result-name">Местонахождение:</div>
+                       <div class="result-text">Казахстан</div>
+                     </div>
+                     <div class="d-flex result justify-content-between">
+                       <div class="result-name">Адрес склада:</div>
+                       <div class="result-text">ТК “Садовод”</div>
+                     </div>
+                     <div class="d-flex result justify-content-between">
+                       <div class="result-name">Прибытие:</div>
+                       <div class="result-text">2 дня</div>
+                     </div>
+                     <div class="d-flex result justify-content-between">
+                       <div class="result-name">Телефоны</div>
+                       <div class="result-phones">
+                         <ul class="list-unstyled">
+                           <li>
+                             <svg width="17" height="17" viewBox="0 0 17 17" fill="none"                                xmlns="http://www.w3.org/2000/svg">
+                                <g clip-path="url(#clip0_353_293)">
+                                <path d="M10.6604 3.54167C11.3523 3.67665 11.9881 4.01502 12.4865 4.51346C12.985 5.0119 13.3233 5.64773 13.4583 6.33959M10.6604 0.708336C12.0978 0.86802 13.4382 1.51171 14.4615 2.53372C15.4848 3.55572 16.1302 4.8953 16.2917 6.3325M15.5833 11.985V14.11C15.5841 14.3073 15.5437 14.5025 15.4647 14.6833C15.3857 14.864 15.2697 15.0263 15.1244 15.1597C14.979 15.293 14.8074 15.3946 14.6205 15.4578C14.4336 15.521 14.2356 15.5444 14.0392 15.5267C11.8595 15.2898 9.76578 14.545 7.92624 13.3521C6.21479 12.2646 4.76377 10.8135 3.67624 9.10209C2.47914 7.25419 1.73417 5.15029 1.50166 2.96084C1.48396 2.76496 1.50724 2.56754 1.57001 2.38115C1.63279 2.19477 1.73369 2.02349 1.86629 1.87823C1.99888 1.73298 2.16027 1.61692 2.34018 1.53746C2.52009 1.45799 2.71457 1.41685 2.91124 1.41667H5.03624C5.38 1.41329 5.71326 1.53502 5.97391 1.75917C6.23455 1.98333 6.4048 2.29461 6.45291 2.635C6.5426 3.31505 6.70894 3.98277 6.94874 4.62542C7.04405 4.87895 7.06467 5.15448 7.00818 5.41938C6.95168 5.68427 6.82044 5.92741 6.62999 6.12L5.73041 7.01959C6.73876 8.79293 8.20707 10.2612 9.98041 11.2696L10.88 10.37C11.0726 10.1796 11.3157 10.0483 11.5806 9.99182C11.8455 9.93532 12.121 9.95595 12.3746 10.0513C13.0172 10.2911 13.6849 10.4574 14.365 10.5471C14.7091 10.5956 15.0233 10.7689 15.248 11.0341C15.4726 11.2992 15.5919 11.6376 15.5833 11.985Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                </g>
+                                <defs>
+                                <clipPath id="clip0_353_293">
+                                <rect width="17" height="17" fill="white"/>
+                                </clipPath>
+                                </defs>
+                            </svg>
+                            <a href="tel: +7574848584455">+7 5748 4858 44 55</a>
+                           </li>
+                           <li>
+                             <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.5294 9.49377C10.2076 9.62525 10.0021 10.1289 9.79361 10.3863C9.68669 10.5181 9.55919 10.5387 9.39484 10.4726C8.18724 9.99148 7.26154 9.18564 6.59515 8.07434C6.48226 7.90201 6.50251 7.76588 6.63865 7.60584C6.83986 7.36877 7.09286 7.09949 7.14732 6.78008C7.26818 6.07352 6.34447 3.88178 5.12458 4.87488C1.61435 7.73533 10.9803 15.3219 12.6707 11.2187C13.1488 10.0556 11.0626 9.27529 10.5294 9.49377ZM8.50001 15.5152C7.25855 15.5152 6.037 15.1851 4.96753 14.5602C4.79587 14.4596 4.58835 14.4331 4.39644 14.4852L2.07255 15.123L2.88204 13.3397C2.99228 13.097 2.96406 12.8144 2.80833 12.5986C1.9424 11.3983 1.48452 9.98119 1.48452 8.5C1.48452 4.6315 4.63152 1.48451 8.50001 1.48451C12.3685 1.48451 15.5152 4.6315 15.5152 8.5C15.5152 12.3682 12.3682 15.5152 8.50001 15.5152V15.5152ZM8.50001 0C3.81306 0 1.25685e-05 3.81305 1.25685e-05 8.5C1.25685e-05 10.1489 0.468177 11.7323 1.35769 13.1066L0.0664188 15.9504C-0.0527804 16.2131 -0.00928429 16.5205 0.177317 16.7394C0.320755 16.907 0.528274 17 0.742434 17C1.22122 17 3.83198 16.1796 4.49638 15.9973C5.72456 16.6544 7.10216 17 8.50001 17C13.1866 17 17 13.1866 17 8.5C17 3.81305 13.1866 0 8.50001 0Z" fill="#39AE41"/>
+                            </svg>
+
+                             <a href="https://www.whatsapp.com/?lang=ru">+7 5748 4858 44 55</a>
+                           </li>
+                         </ul>
+                       </div>
+                     </div>
+                   </div>
+                 </div>
+            </div>
+          </div>
     </div>
   </div>
-
 
 <!-- Including in app.blade.php -->
